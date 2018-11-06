9ab48f91e12faaf35927a22a9c8b7cb0a282c81f
eyasu19833 <44571954+eyasu19833@users.noreply.github.com>
2018-11-06 06:31:00 -0500

    Update InitialCode.py

--- InitialCode.py
+++ InitialCode.py
@@ -37,6 +37,7 @@ print("Top terms per cluster:")
 order_centroids = model.cluster_centers_.argsort()[:, ::-1]
 terms = vectorizer.get_feature_names()
 for i in range(true_k):
-    print "Cluster %d:" % i,
+    print ("Cluster " + i)
     for ind in order_centroids[i, :10]:
-        print ' %s' % terms[ind],
+        print  (terms[ind])
+	
